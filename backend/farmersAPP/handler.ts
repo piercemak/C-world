8ab17a9a4fe384abc40ea753e6a7f69131b65a59
Pierce Makombe <piercefmak@gmail.com>
2024-10-24 09:48:29 -0400

    Updated CORS policy for new custom domain

--- backend/farmersAPP/handler.ts
+++ backend/farmersAPP/handler.ts
@@ -34,7 +34,7 @@ export async function main(event: APIGatewayEvent) {
     return {
         statusCode: 200,
         headers: {
-            ["Access-Control-Allow-Origin"]: "http://localhost:3000"
+            ["Access-Control-Allow-Origin"]: "fanciful-biscotti-b7ec51.netlify.app"
         },
         body: result
     }
