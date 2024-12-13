4dcf25276836f6d5a7d4de5218c0f053fb106d49
Pierce Makombe <piercefmak@gmail.com>
2024-12-12 20:37:24 -0500

    Swapped Iphone.svg image on Farmers Mobile 2

--- websitemockups/src/app/components/ContentSection3.jsx
+++ websitemockups/src/app/components/ContentSection3.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { React, useState, useEffect, useRef } from 'react'
 import { motion, useMotionValue, useTransform, animate } from "framer-motion";
-import Iphone from './farmersImages/Iphone.svg'
+import Iphone from '../farmersImages/Iphone.svg'
 
 const ContentSection3 = ({ toggleChat }) => {
     const rigthArrow = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6"><path fillRule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" /></svg>
