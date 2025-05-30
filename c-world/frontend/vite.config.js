3cae4e67bbdb0bc92b953e5ed8656a86fc291797
Pierce Makombe <piercefmak@gmail.com>
2025-05-30 00:51:47 -0400

    CORS settings

--- c-world/frontend/vite.config.js
+++ c-world/frontend/vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: ['c-world-frontend.onrender.com'],
+    allowedHosts: ['cearaworld.com', 'www.cearaworld.com'],
   },  
 })
