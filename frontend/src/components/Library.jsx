30c689aecafa0c222050aae4c005ac3effe8144b
Pierce Makombe <piercefmak@gmail.com>
2025-08-08 16:37:00 -0400

    Added season 3-5 episode titles

--- frontend/src/components/Library.jsx
+++ frontend/src/components/Library.jsx
@@ -745,9 +745,38 @@ const Library = () => {
         "The_First_Homunculus",
         "Conflict_at_Baschool",
         "Daydream"
-      ]      
+      ],
 
-
+      4: [
+        "Homunculus",
+        "The_Abyss",
+        "Signs_of_a_Counteroffensive",
+        "Bite_of_the_Ant",
+        "Revving_at_Full_Throttle",
+        "The_Promised_Day",
+        "Looming_Shadows",
+        "Emissary_of_Darkness",
+        "The_Oath_in_the_Tunnel",
+        "Filial_Affection",
+        "Upheaval_in_Central",
+        "The_Immortal_Legion",
+        "Combined_Strength"
+      ],
+      
+      5: [
+        "Flame_of_Vengeance",
+        "Beyond_the_Inferno",
+        "The_Adults_Way_of_Life",
+        "The_Return_of_the_Fuhrer",
+        "Eternal_Leave",
+        "Sacrifices",
+        "Lost_Light",
+        "Eye_of_Heaven_Gateway_of_Earth",
+        "He_Who_Would_Swallow_God",
+        "A_Fierce_Counterattack",
+        "The_Other_Side_of_the_Gateway",
+        "Journeys_End"
+      ]                  
     }   
 
 
