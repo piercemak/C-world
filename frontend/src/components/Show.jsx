1272aac46ec3ae4d62dd42331e15a51b981d60c2
piercemak <113068398+piercemak@users.noreply.github.com>
2025-11-27 20:53:09 -0500

    Update season 1 episode 3 intro and outro times

--- frontend/src/components/Show.jsx
+++ frontend/src/components/Show.jsx
@@ -369,7 +369,7 @@ if (prevEpisode < 1) {
         1: {
           1: { outro: { start: 3291, skipTo: "next" } },
           2: { intro: { start: 455.0, end: 477.0 }, outro: { start: 3600, skipTo: "next" } },
-          3: { intro: { start: 131.0, end: 211.0 }, outro: { start: 3629, skipTo: "next" } },
+          3: { intro: { start: 385.0, end: 407.0 }, outro: { start: 2516, skipTo: "next" } },
           4: { intro: { start: 384.0, end: 406.0 }, outro: { start: 2490, skipTo: "next" } },
         }
       }
