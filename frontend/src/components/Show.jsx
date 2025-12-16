ae75c6b65c77d537448b015ed63e5b20d86abae6
Pierce Makombe <piercefmak@gmail.com>
2025-12-16 17:36:26 -0500

    Fix for Safari outro skipping

--- frontend/src/components/Show.jsx
+++ frontend/src/components/Show.jsx
@@ -21,6 +21,11 @@ const Show = ({ src, delayPlay = 0, onSkipToNext, showId, season, episode, skipI
   const nextepIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="size-6" viewBox="0 0 16 16"><path d="M12.5 4a.5.5 0 0 0-1 0v3.248L5.233 3.612C4.693 3.3 4 3.678 4 4.308v7.384c0 .63.692 1.01 1.233.697L11.5 8.753V12a.5.5 0 0 0 1 0z"/></svg>
   const prevepIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="size-6" viewBox="0 0 16 16"><path d="M4 4a.5.5 0 0 1 1 0v3.248l6.267-3.636c.54-.313 1.232.066 1.232.696v7.384c0 .63-.692 1.01-1.232.697L5 8.753V12a.5.5 0 0 1-1 0z"/></svg>
 
+  {/* Outro Ref */}
+  const outroSkipRef = useRef(false);
+  useEffect(() => {
+    outroSkipRef.current = false;
+  }, [src]);
 
   {/* Volume Control */}
   const [volumeHovered, setvolumeHovered] = useState(false);
@@ -719,6 +724,11 @@ const hasIntro = !!(intro && Number.isFinite(intro.end));
   };
 
 const handleSkipOutro = async () => {
+  if (outroSkipRef.current) return;
+  outroSkipRef.current = true;
+  setCountdown(null);
+  setOutroVisible(false);
+
   if (outro?.skipTo === "next") {
     const opts = { source: "outro" };
     if (getSignedUrl && typeof getSignedUrl === "function") {
