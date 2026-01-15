7a6a71a93c4aba460045aae227a28159d0747c37
Pierce Makombe <piercefmak@gmail.com>
2026-01-15 17:23:50 -0500

    added intro/outro times for jjk s3e3

--- frontend/src/components/Show.jsx
+++ frontend/src/components/Show.jsx
@@ -357,6 +357,7 @@ if (prevEpisode < 1) {
         3: {
           1: { intro: { start: 222.0, end: 313.0 }, outro: { start: 1329, skipTo: "next" } },
           2: { intro: { start: 0.0, end: 90.0 }, outro: { start: 1325, skipTo: "next" } },
+          3: { intro: { start: 0.0, end: 90.0 }, outro: { start: 1328, skipTo: "next" } },
         }
       }
     },
