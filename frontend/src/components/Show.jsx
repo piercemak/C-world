01abdfb994f8e9785bea29c07ae5c3c47ad02c20
Pierce Makombe <piercefmak@gmail.com>
2026-02-04 19:12:21 -0500

    added jjk s3e5 again

--- frontend/src/components/Show.jsx
+++ frontend/src/components/Show.jsx
@@ -179,7 +179,7 @@ const Show = ({ src, delayPlay = 0, onSkipToNext, showId, season, episode, skipI
     jjk: {
       1:24,
       2:23,
-      3:2,
+      3:5,
     }, 
       cyberpunk: {
       1:10,
