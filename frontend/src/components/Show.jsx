ad914b4cfb1992eaca882026344a934dbf8181f7
Pierce Makombe <piercefmak@gmail.com>
2026-02-12 19:50:05 -0500

    added jjk s3e7 subtitles

--- frontend/src/components/Show.jsx
+++ frontend/src/components/Show.jsx
@@ -179,7 +179,7 @@ const Show = ({ src, delayPlay = 0, onSkipToNext, showId, season, episode, skipI
     jjk: {
       1:24,
       2:23,
-      3:6,
+      3:7,
     }, 
       cyberpunk: {
       1:10,
@@ -361,7 +361,7 @@ if (prevEpisode < 1) {
           4: { intro: { start: 0.0, end: 90.0 }, outro: { start: 1585, skipTo: "next" } },
           5: { intro: { start: 380.0, end: 470.0 }, outro: { start: 1330, skipTo: "next" } },
           6: { intro: { start: 198.0, end: 288.0 }, outro: { start: 1330, skipTo: "next" } },
-          7: { intro: { start: 198.0, end: 288.0 }, outro: { start: 1330, skipTo: "next" } },
+          7: { intro: { start: 183.0, end: 273.0 }, outro: { start: 1328, skipTo: "next" } },
         }
       }
     },
