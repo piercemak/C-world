bbdfdec34b4b3a7b8082c8b96a6cc5ff62b22ba3
Pierce Makombe <piercefmak@gmail.com>
2026-02-25 17:40:27 -0500

    chore(db): use Render DATABASE_URL with sqlite fallback

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv # type: ignore
+import dj_database_url # type: ignore
 import os
 from django.core.exceptions import ImproperlyConfigured
 
@@ -128,12 +129,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=False,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
