9c6bddbb27b4081ea3cfa3198095312686b083fd
Pierce Makombe <piercefmak@gmail.com>
2026-02-26 14:04:40 -0500

    added jjk s3e8

--- frontend/src/components/newMedia.js
+++ frontend/src/components/newMedia.js
@@ -3,6 +3,16 @@
     const clean = (s) => (s || "").replace(/-/g, "");
     export const newMedia = [
       {
+        kind: "episode",
+        showSlug: "jjk",                   
+        showTitle: "Jujutsu Kaisen",
+        season: 3,
+        episode: 8,
+        episodeTitle: "Tokyo Colony No 2",
+        placeholder: `${cloudFrontDomain}/${clean("jjk")}/placeholders/season3/S3E8_${clean("jjk")}_placeholder.png`,
+        to: `/video-library/jjk?season=3&episode=8`,
+      }, 
+      {
         kind: "movie",
         showSlug: "bugonia",
         showTitle: "Bugonia",
@@ -38,15 +48,5 @@
         episodeTitle: "Fever",
         placeholder: `${cloudFrontDomain}/${clean("jjk")}/placeholders/season3/S3E5_${clean("jjk")}_placeholder.png`,
         to: `/video-library/jjk?season=3&episode=5`,
-      },      
-      {
-        kind: "episode",
-        showSlug: "jjk",                   
-        showTitle: "Jujutsu Kaisen",
-        season: 3,
-        episode: 4,
-        episodeTitle: "Perfect Preparation",
-        placeholder: `${cloudFrontDomain}/${clean("jjk")}/placeholders/season3/S3E4_${clean("jjk")}_placeholder.png`,
-        to: `/video-library/jjk?season=3&episode=4`,
-      },      
+      },           
     ];
